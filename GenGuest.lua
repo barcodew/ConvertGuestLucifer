ab109fa2f1ce0d0dad46da494b25d84549494aa9
Barcodew <114000864+barcodew@users.noreply.github.com>
2024-01-04 06:28:50 +0800

    Update GenGuest.lua

--- GenGuest.lua
+++ GenGuest.lua
@@ -1,13 +1,48 @@
 local growid = {"growid"}
 local pass = {"!passwordbuluk2kali"}
 local email = {
-    "ratkhacnhai82@gmail.com",
-"nhapdongn@gmail.com",
-"shelleyroberson023@gmail.com"
+    "hauwauyunusa0000@gmail.com",
+"corurua838@gmail.com",
+"ouafinabila1@gmail.com"
 }
+
+totalBot = 3
+
 RandomPW = true 
 RandomName = true 
-totalBot = 3
+
+local file = io.open("resultCIDByXcoBar.txt", "w")
+local bot = getBot() 
+bot.auto_tutorial = true 
+local a = 0
+for i,bots in ipairs(getBots()) do
+    a = a+1
+    if bots.name == bot.name then
+        break
+    end
+end
+
+local function reconBots(reconnect)
+    if getBot().status ~= 1 then
+        sleep(10000)
+        getBot().connext(true)
+    end
+end
+local function reconAAP(checkAAP)
+    if getBot().status ~= "AAP" then
+        sleep(10000)
+        getBot().connext(true)
+    end
+    if getBot().status ~= "AAP" then
+        sleep(10000)
+        getBot().connext(true)
+    end
+    if getBot().status ~= "AAP" then
+        sleep(10000)
+        getBot().connext(true)
+    end
+end
+
 
 local function RandomGen(LetterWorld)
         local word = ""
@@ -22,23 +57,10 @@ local function RandomGen(LetterWorld)
     return word    
 end
 
-local function reconBots()
-    while getBot().status ~= 1 do
-        getBot():connect(true)
-        sleep(10000)
-    end
-end
-local function reconAAP()
-    while getBot().status ~= "AAP" do
-        getBot():connect(true)
-        sleep(10000)
-    end
-end
-
-
-
-local function createId()
     sleep(1000)
+    bot.auto_tutorial = false
+    local randomGrowid = RandomGen(10)
+    local randomPass = RandomGen(7).."@";
     getBot():sendPacket(2, "action|growid")
     sleep(5000)
     if RandomPW and RandomName == false then 
@@ -47,53 +69,25 @@ local function createId()
     getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..randomGrowid.."\npassword|"..pass[a].."\npassword_verify|"..pass[a].."\nemail|"..email[a])
     end
     if RandomName and RandomPW then
-        getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..randomGrowid.."\npassword|1!"..randomPass.."\npassword_verify|1!"..randomPass.."\nemail|"..email[a])
+    getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..randomGrowid.."\npassword|1!"..randomPass.."\npassword_verify|1!"..randomPass.."\nemail|"..email[a])
     end
     if RandomPW == false and RandomName == false then 
 
         getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..growid[a].."\npassword|"..pass[a].."\npassword_verify|"..pass[a].."\nemail|"..email[a])
     end
-end
 
-local function result()
-    getBot():connect(true)
-    if getBot().status == 6 then
-        file:write("----------------------------\n")
-        file:write("==== Create By Barcodew ====\n")
-        file:write("----------------------------\n")
-        file:write(randomGrowid..":"..randomPass..":"..email[a])
-        
-    end
-end
 
-local function joinWorld()
-        getBot():sendPacket(2,"action|join_request\nname|" .."FAKELUMINOUS".."\ninvitedWorld|0")
-end
 
-local bot = getBot() 
-bot.auto_tutorial = true 
-bot.auto_tutorial = false
-local file = io.open("resultCIDByXcoBar.txt", "w")
-
-local randomPass = RandomGen(7).."@"
-local randomGrowid = RandomGen(10)
-
-local a = 0
-for i,bots in ipairs(getBots()) do
-    a = a+1
-    if bots.name == bot.name then
-        break
-    end
-end
 reconBots()
-joinWorld()
-createId()
 reconAAP()
-result()
+if getBot().status == "AAP" then
+    file:write(randomGrowid.."|"..randomPass.."|"..email[a])
+end
+
 if a == totalBot then
     file:close()
+    print("Succesfully Create")
 end
-
 
 
 
