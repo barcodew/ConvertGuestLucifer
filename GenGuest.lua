5027714c074c6b50e56220e3ff8afa53afc98ff8
Barcodew <114000864+barcodew@users.noreply.github.com>
2024-01-04 04:04:27 +0800

    Update GenGuest.lua

--- GenGuest.lua
+++ GenGuest.lua
@@ -8,7 +8,23 @@ local email = {
 RandomPW = true 
 RandomName = true 
 
+local file = io.open("resultCIDByXcoBar.txt", "w")
+local bot = getBot() 
+bot.auto_tutorial = true 
+local a = 0
+for i,bots in ipairs(getBots()) do
+    a = a+1
+    if bots.name == bot.name then
+        break
+    end
+end
 
+local function reconBots(){
+    if getBot().status ~= 1 then
+        sleep(10000)
+        getBot().connext(true)
+    end
+}
 
 
 local function RandomGen(LetterWorld)
@@ -24,51 +40,35 @@ local function RandomGen(LetterWorld)
     return word    
 end
 
-local file = io.open("resultCIDByXcoBar.txt", "w")
-local randomPass = RandomGen(7).."@"
-local randomGrowid = RandomGen(10)
-local function createId()
+local function createId(){
     sleep(1000)
     bot.auto_tutorial = false
+    local randomPass = RandomGen(7).."@";
     getBot():sendPacket(2, "action|growid")
     sleep(5000)
     if RandomPW and RandomName == false then 
     getBot():sendPacket(2,"action|dialog_return\ndialog_name|growid_apply\nlogon|"..growid[a].."\npassword|"..pass[a].."\npassword_verify|"..pass[a].."\nemail|"..email[a])
     elseif RandomPW == false and RandomName then 
-    getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..randomGrowid.."\npassword|"..pass[a].."\npassword_verify|"..pass[a].."\nemail|"..email[a])
+    getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..RandomGen(10).."\npassword|"..pass[a].."\npassword_verify|"..pass[a].."\nemail|"..email[a])
     end
     if RandomName and RandomPW then
-        print(getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..randomGrowid.."\npassword|1!"..randomPass.."\npassword_verify|1!"..randomPass.."\nemail|"..email[a]))
-        getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..randomGrowid.."\npassword|1!"..randomPass.."\npassword_verify|1!"..randomPass.."\nemail|"..email[a])
-        print(getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..randomGrowid.."\npassword|1!"..randomPass.."\npassword_verify|1!"..randomPass.."\nemail|"..email[a]))
+    getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..RandomGen(10).."\npassword|1!"..randomPass.."\npassword_verify|1!"..randomPass.."\nemail|"..email[a])
     end
     if RandomPW == false and RandomName == false then 
 
         getBot():sendPacket(2, "action|dialog_return\ndialog_name|growid_apply\nlogon|"..growid[a].."\npassword|"..pass[a].."\npassword_verify|"..pass[a].."\nemail|"..email[a])
     end
-end
+}
 
 
-local bot = getBot() 
-bot.auto_tutorial = true 
-local a = 0
-for i,bots in ipairs(getBots()) do
-    a = a+1
-    if bots.name == bot.name then
-        createId()
-        getBots():connect(true)
-        break
-    end
-end
 
-
+reconBots()
+createId()
+getBot().connext(true)
 if getBot().status == "AAP" then
     file:write("----------------------------\n")
     file:write("==== Create By Barcodew ====\n")
     file:write("----------------------------\n")
     file:write(RandomName..":"..randomPass..":"..email[a])
-    
+    file:close()
 end
-
-
-file:close()
